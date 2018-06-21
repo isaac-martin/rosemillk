b48b3ed1534840fefe4ad5be5318495e3b1734e9
isaac-martin <isaac@weareplayground.co>
2018-06-21 18:16:00 -0400

    still not

--- src/components/Collection.js
+++ src/components/Collection.js
@@ -2,11 +2,11 @@ import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 // import Client from 'shopify-buy';
 
-const Collection = ({title, handle, id, ...props}) => (
+const Collection = ({attrs: {title, handle, id}, ...props}) => (
   <Link to={`/collection/${handle}`} className="absolute absolute--fill z-3">
     <div id={id}>
       <div className="pv3 bb b--light-gray flex justify-between items-center">
-        <h1 className="f6 mv0 black ttu biryani pr2"> Collection : {title}</h1>
+        <h1 className="f6 mv0 black ttu biryani pr2"> Collection{title}</h1>
       </div>
     </div>
   </Link>
