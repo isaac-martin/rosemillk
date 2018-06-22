2e30ed719764065cfd7ea70b1e96e53044691f48
isaac-martin <isaac@weareplayground.co>
2018-06-22 13:01:49 -0400

    Added files

--- src/components/CollectionArchive.js
+++ src/components/CollectionArchive.js
@@ -8,11 +8,19 @@ import actions from '.././store/actions';
 class CollectionArchive extends Component {
     componentDidMount() {
         const {setProducts, setCollectionID, attrs} = this.props;
-        setCollectionID(attrs.handle.value);
+        setCollectionID(attrs.id.value);
     }
 
     render() {
-        return <div className="p3 ph4-l pad-bottom">Collection Title : </div>;
+        // const {
+        //     attrs: {title, id}
+        // } = this.props;
+        return (
+            <div className="p3 ph4-l pad-bottom">
+                Collection Title :
+                {/* {title.value} */}
+            </div>
+        );
     }
 }
 
