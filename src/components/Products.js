6381d3469f6f377f23506fff6637d3b9d3fc7764
isaac-martin <isaac@weareplayground.co>
2018-11-18 17:00:56 -0500

    added photos

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Product from './Product';
+import {CSSTransition} from 'react-transition-group';
 
 class Products extends Component {
   render() {
-    let products = this.props.products.map((product) => {
-      return (
-        <Product
-          client={this.props.client}
-          key={product.id.toString()}
-          product={product}
-        />
-      );
+    let products = this.props.products.map(product => {
+      return <Product client={this.props.client} key={product.id.toString()} product={product} />;
     });
 
     return (
       <div className="Product-wrapper">
-        {products}
+        <CSSTransition in={true} appear={true} timeout={1000} classNames="fade">
+          {/* <CSSTransition appear={true} enter={true} className="productList"> */}
+          {products}
+        </CSSTransition>
       </div>
     );
   }
