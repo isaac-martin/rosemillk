1d5d177e82838da47e197e5082ce653d5b8f3b44
Isaac Martin <isaac@weareplayground.co>
2019-06-28 10:00:30 -0700

    remove log

--- src/util.js
+++ src/util.js
@@ -1,6 +1,5 @@
 export function bodyCol(old) {
   const body = document.querySelector('body');
-  console.log(old);
   const cols = ['bg-pink', 'bg-org', 'bg-lilac','bg-rose','bg-sapphire','bg-peach', 'bg-org','bg-blush','bg-teal'];
   const classes = cols.filter(col => col !== old);
   body.classList.remove(...cols);
